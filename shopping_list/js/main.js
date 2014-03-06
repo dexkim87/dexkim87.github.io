9beefa97707725d1086bab4c7caa81b2a1a59487
Dexter <dexkim87@gmail.com>
2014-03-06 11:08:02 -0500

    Push 3

--- shopping_list/js/main.js
+++ shopping_list/js/main.js
@@ -1,18 +1,19 @@
 $(document).ready(function() {
-	$("#submit").click(function(event){
+		$("#submit").click(function(event){
 	    event.preventDefault();
 	    var input = $("#input_field").val();
         var inputlength = $("#input_field").val().length;
 	    if (inputlength>0) {
-		$("#fears_list").append("<a class='fear_list_icon'></a><div class='fear_list_item'>"+input+"</div>");
+		$("#fears_list").append("<a href='#' class='fear_list_icon'></a><div class='fear_list_item'>"+input+"</div>");
+		 $("a.fear_list_icon").click(function(){
+	    $(this).toggleClass("checked_box");
+	    $(this).parent().toggleClass("checked_text");
+	    console.log("hi");
+    })
+
 		}
 		else {
 			alert("Do you fear nothing? Type something before submitting!")
 		}
    	})
-	    $("a.fear_list_icon").click(function(){
-	    $(this).toggleClass("checked_box");
-	    $(this).parent().toggleClass("checked_text");
-	    console.log("hi");
-    });
 });
